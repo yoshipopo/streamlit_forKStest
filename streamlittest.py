f51ba79e5a567aa96f47b36a6bf474ee09a583a4
yoshipopo <109138096+yoshipopo@users.noreply.github.com>
2022-07-14 00:43:21 +0900

    Update streamlittest.py

--- streamlittest.py
+++ streamlittest.py
@@ -24,7 +24,12 @@ def main():
   selections = st.multiselect('銘柄を複数選択してください',df_all_company_list['コード&銘柄名'],
                               ['8306三菱ＵＦＪフィナンシャル・グループ','8591オリックス','9020東日本旅客鉄道','9101日本郵船'])
   st.write('選択した銘柄')
+  
+  st.dataframe(selections_to_selected_company_list_and_selected_company_list_hyouji(df_all_company_list,selections)[0])
 
+  selected_company_list = selections_to_selected_company_list_and_selected_company_list_hyouji(df_all_company_list,selections)[1]
+  selected_company_list_hyouji = selections_to_selected_company_list_and_selected_company_list_hyouji(df_all_company_list,selections)[2]
+  selected_company_list_hyouji_datenashi = selections
   
 
 def session_change():
@@ -36,6 +41,14 @@ def path_to_df_all_company_list(path):
     df_all_company_list = df_all_company_list.replace('-', np.nan)
     df_all_company_list['コード&銘柄名'] = df_all_company_list['コード'].astype(str)+df_all_company_list['銘柄名']
     return df_all_company_list
+  
+def selections_to_selected_company_list_and_selected_company_list_hyouji(df_all_company_list,selections):
+    df_meigarasenntaku_temp = df_all_company_list[df_all_company_list['コード&銘柄名'].isin(selections)]
+    selected_company_list = [str(i)+'.JP' for i in df_meigarasenntaku_temp['コード']]
+    d = deque(selections)
+    d.appendleft('Date')
+    selected_company_list_hyouji = list(d)
+    return df_meigarasenntaku_temp, selected_company_list, selected_company_list_hyouji
   
 if __name__ == "__main__":
     main()
